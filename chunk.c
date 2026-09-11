38390fc23004fa2daac7ed89ace4cade2e024031
Sourav Maity <rootedsourav@gmail.com>
2026-09-11 20:34:03 +0530

    added memory.c

--- chunk.c
+++ chunk.c
@@ -2,7 +2,7 @@
 #include "memory.h"
 #include <stddef.h>
 
-void initChunk(Chunk *chunk) {
+void initChunk(Chunk* chunk) {
     chunk -> count = 0;
     chunk -> capacity = 0;
     chunk -> code = NULL;
